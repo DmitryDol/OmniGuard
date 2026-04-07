cc72874b9ee0f4cb863325269a65162c9257a3ca
DmitryDol <ddimapalih@gmail.com>
2026-04-07 21:28:27 +0400

    Refactor person detection logic to improve code readability and maintainability. Updated zone handling and detection confidence threshold to utilize configuration settings. Enhanced docstring formatting for clarity.

--- personDetector.py
+++ personDetector.py
@@ -1,42 +1,53 @@
-
 import cv2
 import numpy as np
 import supervision as sv
 import torch
 
+from config import settings
+
 
 class Detector:
-    def __init__ (self, resolution: tuple[int,int], polygons_arr: list ):
+    def __init__(self, resolution: tuple[int, int], polygons_arr: list):
         self.resolution = resolution
-        self.model =  torch.hub.load('ultralytics/yolov5', 'yolov5n', pretrained=True)
+        self.model = torch.hub.load("ultralytics/yolov5", "yolov5n", pretrained=True)
         self.model.classes = [0]
-        self.box_annotator = sv.BoundingBoxAnnotator(thickness=4)
+        self.box_annotator = sv.BoxAnnotator(thickness=4)
 
         # для каждой зоны
         self.zone_arr = []
         self.annotators = []
 
         for p_cords in polygons_arr:
-            cords = np.array(p_cords).reshape(len(p_cords)//2,2)
-            p_zone = sv.PolygonZone(polygon=cords, frame_resolution_wh=self.resolution)
+            cords = np.array(p_cords).reshape(len(p_cords) // 2, 2)
+            p_zone = sv.PolygonZone(polygon=cords)
             self.zone_arr.append(p_zone)
-            self.zone_annotator = sv.PolygonZoneAnnotator(zone=p_zone, color=sv.Color.GREEN, \
-                                                        thickness=2, text_thickness=1, text_scale=1 )
+            self.zone_annotator = sv.PolygonZoneAnnotator(
+                zone=p_zone,
+                color=sv.Color.GREEN,
+                thickness=2,
+                text_thickness=1,
+                text_scale=1,
+            )
             self.annotators.append(self.zone_annotator)
-        
+
     def change_zone(self, polygons_arr: np.ndarray):
         self.zone_arr = []
         self.annotators = []
         for p_cords in polygons_arr:
-            cords = np.array(p_cords).reshape(len(p_cords)//2,2)
-            p_zone = sv.PolygonZone(polygon=cords, frame_resolution_wh=self.resolution)
+            cords = np.array(p_cords).reshape(len(p_cords) // 2, 2)
+            p_zone = sv.PolygonZone(polygon=cords)
             self.zone_arr.append(p_zone)
-            self.zone_annotator = sv.PolygonZoneAnnotator(zone=p_zone, color=sv.Color.GREEN, \
-                                                        thickness=2, text_thickness=1, text_scale=1 )
+            self.zone_annotator = sv.PolygonZoneAnnotator(
+                zone=p_zone,
+                color=sv.Color.GREEN,
+                thickness=2,
+                text_thickness=1,
+                text_scale=1,
+            )
             self.annotators.append(self.zone_annotator)
-    
+
     def detect(self, frame) -> tuple:
-        '''
+        """
         Detects people in the zone
 
         args:
@@ -45,18 +56,20 @@ class Detector:
         return:
                 was a person detected in the zone
 
-        '''
-        
-        #frame = cv2.resize(frame, self.resolution)
+        """
+
+        # frame = cv2.resize(frame, self.resolution)
         results = self.model(frame)
         detections = sv.Detections.from_yolov5(results)
-        detections = detections[detections.confidence > 0.4]
+        detections = detections[
+            detections.confidence > settings.DETECTION_CONFIDENCE_THRESHOLD
+        ]
         people = 0
         for zone, annotator in zip(self.zone_arr, self.annotators):
             trig_arr = zone.trigger(detections)
             people += np.sum(trig_arr == True)
             frame = annotator.annotate(scene=frame)
-            
+
         frame = self.box_annotator.annotate(scene=frame, detections=detections)
 
         return (people > 0, frame, self.box_annotator, self.annotators, detections)
